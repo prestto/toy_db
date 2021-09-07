183e0ba924c8bb760c77e81f2f8d8a8c58149ae4
Tom PRESTON <t.preston@nodya-group.com>
2021-09-07 16:37:36 +0200

    added pgadmin and clean + psql commands to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cecho() {
-    LB='\033[1;36m'    # Light Blue
-    LG='\033[1;32m'    # Light Green
-    YE='\033[1;33m'    # Yellow
+    LB='\033[1;36m' # Light Blue
+    LG='\033[1;32m' # Light Green
+    YE='\033[1;33m' # Yellow
 
     # print normally if no second arg added
     if [[ $2 == "" ]]; then
@@ -26,30 +26,59 @@ cecho() {
     esac
 }
 
-function launch_postgres {
-    cecho "BL" "Hello, world!"
-
+function launch_all {
+    cecho "BL" "Running postgres & pgadmin..."
+    docker-compose --project-directory . -f docker/docker-compose-postgres.yml -f docker/docker-compose-pgadmin.yml up -d
 }
 
+function launch_postgres {
+    cecho "BL" "Running postgres..."
+    docker-compose --project-directory . -f docker/docker-compose-postgres.yml up -d
+}
+
+function run_clean {
+    cecho "BL" "Stopping containers."
+    docker-compose --project-directory . -f docker/docker-compose-postgres.yml stop
+
+    cecho "BL" "Removing containers."
+    docker rm toy_db_postgresql_1 || cecho "BL" "No containers found" 
+}
+
+function run_psql {
+    cecho "docker" "-> Connecting psql"
+    docker exec -it toy_db_postgresql_1 psql -U pguser -d toy_db
+}
 
 function show_help {
-        echo "Help: $0 <ACTION>"
-        echo "Parameters :"
-        echo " - ACTION values :"
-        echo "   * up                            - Run postgres and pgadmin."
+    cecho "Help: $0 <ACTION>"
+    cecho "Parameters :"
+    cecho " - ACTION values :"
+    cecho "   * up                            - Run postgres and pgadmin."
+    cecho "   * pg                            - Run postgres only."
+    cecho "   * psql                          - Run psql in the postgres container."
+    cecho "   * clean                         - Remove postgres and pgadmin containers."
 }
 
 if [[ "$1" == "" ]]; then
-   echo "No arguments provided."
-   show_help
-   exit 1
+    cecho "No arguments provided."
+    show_help
+    exit 1
 fi
 
 case "$1" in
-        up)
-                launch_postgres
-                ;;
-        *)
-                show_help
-                ;;
+up)
+    launch_all
+    ;;
+up)
+    launch_postgres
+    ;;
+psql)
+    run_psql
+    ;;
+clean)
+    run_clean
+    ;;
+*)
+    show_help
+    ;;
 esac
